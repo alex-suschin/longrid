669584727f547a526f582ce987d627a2db1212f9
Alex Suschin <alex_suschin@creativepeople.agency>
2021-09-23 15:58:51 +0300

    dist-open

---
+++ precache-manifest.00ac3f47aace5615e5e9a82321ed6398.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "082974e0d8a7a9bf1513",
+    "url": "/css/app.6e0b904d.css"
+  },
+  {
+    "revision": "0a58abfa28d66f0a065c1ffb13b7601f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "082974e0d8a7a9bf1513",
+    "url": "/js/app.4e8eeff4.js"
+  },
+  {
+    "revision": "601dfadf84bf0d082c21",
+    "url": "/js/chunk-vendors.600e4e28.js"
+  },
+  {
+    "revision": "cd5ec30f1294958c2a2aea316a2530cf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
